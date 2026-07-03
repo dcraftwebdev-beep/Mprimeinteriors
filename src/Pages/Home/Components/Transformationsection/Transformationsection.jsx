55c92ed2895bf2a247f28015d2d97c18168e865d
Dinesh Babu <dcraftwebdev@gmail.com>
2026-07-03 17:19:55 +0530

    import fix

--- src/Pages/Home/Components/Transformationsection/Transformationsection.jsx
+++ src/Pages/Home/Components/Transformationsection/Transformationsection.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import styles from "./TransformationSection.module.css";
+import styles from "./Transformationsection.module.css";
 
 gsap.registerPlugin(ScrollTrigger);
 
